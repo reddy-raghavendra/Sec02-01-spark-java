ad1de9d89cfe310c74a11ebc1d3d497a53b1984a
Kurra <S542409@nwmissouri.edu>
2022-04-19 22:09:17 -0500

    modified class changes in ranked and voting page

--- Harshavardhan Kurra/pagerank-kurra-java/src/main/java/edu/nwmsu/sec02grp1/kurra/RankedPageKurra.java
+++ Harshavardhan Kurra/pagerank-kurra-java/src/main/java/edu/nwmsu/sec02grp1/kurra/RankedPageKurra.java
@@ -1,13 +1,35 @@
 package edu.nwmsu.sec02grp1.kurra;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RankedPageKurra {
+public class RankedPageKurra implements Serializable{
     String voter;
     ArrayList<VotingPageKurra> voterList = new ArrayList<>();
     
     public RankedPageKurra(String voter, ArrayList<VotingPageKurra> voters){
         this.voter = voter;
         this.voterList = voters;
+    }
+    
+    public String getVoter() {
+        return voter;
+    }
+
+    public void setVoter(String voter) {
+        this.voter = voter;
+    }
+
+    public ArrayList<VotingPageKurra> getVoterList() {
+        return voterList;
+    }
+
+    public void setVoterList(ArrayList<VotingPageKurra> voterList) {
+        this.voterList = voterList;
+    }
+
+    @Override
+    public String toString(){
+        return voter + voterList;
     }
 }
