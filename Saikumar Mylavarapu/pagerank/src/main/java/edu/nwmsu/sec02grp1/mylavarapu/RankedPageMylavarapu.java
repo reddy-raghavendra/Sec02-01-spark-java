78edd2abf4abb17a3027c682edf5b02268acc637
saikumar438 <saikumar.mylavarapu1996@gmail.com>
2022-04-24 22:47:36 -0500

    modified all the files

--- Saikumar Mylavarapu/pagerank/src/main/java/edu/nwmsu/sec02grp1/mylavarapu/RankedPageMylavarapu.java
+++ Saikumar Mylavarapu/pagerank/src/main/java/edu/nwmsu/sec02grp1/mylavarapu/RankedPageMylavarapu.java
@@ -1,13 +1,19 @@
 package edu.nwmsu.sec02grp1.mylavarapu;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RankedPageMylavarapu {
+public class RankedPageMylavarapu implements Serializable{
     String voter;
     ArrayList<VotingPageMylavarapu> voterList = new ArrayList<>();
     
     public RankedPageMylavarapu(String voter, ArrayList<VotingPageMylavarapu> voters){
         this.voter = voter;
         this.voterList = voters;
+    }
+
+    @Override
+    public String toString() {
+        return voter + voterList;
     }
 }
--- Saikumar Mylavarapu/pagerank/src/main/java/edu/nwmsu/sec02grp1/mylavarapu/VotingPageMylavarapu.java
+++ Saikumar Mylavarapu/pagerank/src/main/java/edu/nwmsu/sec02grp1/mylavarapu/VotingPageMylavarapu.java
@@ -1,12 +1,13 @@
 package edu.nwmsu.sec02grp1.mylavarapu;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.values.KV;
 
-public  class VotingPageMylavarapu extends DoFn<KV<String,Iterable<String>>,KV<String,RankedPageMylavarapu>>{
+public  class VotingPageMylavarapu extends DoFn<KV<String,Iterable<String>>,KV<String,RankedPageMylavarapu>> implements Serializable{
     String voterName;
     int contributorVotes;
     public VotingPageMylavarapu(String voterName,Integer contributorVotes2){
