0b68f2bc0f1d17e51651c679baedbaa26d1438c4
SushmaNalla <77800401+SushmaNalla@users.noreply.github.com>
2022-04-25 22:04:17 -0500

    Job2 is finished

--- Sushma Nalla/pagerank-nalla-java/src/main/java/edu/nwmsu/sec02grp1/nalla/RankedPageNalla.java
+++ Sushma Nalla/pagerank-nalla-java/src/main/java/edu/nwmsu/sec02grp1/nalla/RankedPageNalla.java
@@ -1,13 +1,45 @@
 package edu.nwmsu.sec02grp1.nalla;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RankedPageNalla {
+public class RankedPageNalla implements Serializable {
     String voter;
+    double rank = 1.0;
     ArrayList<VotingPageNalla> voterList = new ArrayList<>();
     
+    public RankedPageNalla(String voter,double rank, ArrayList<VotingPageNalla> voters){
+        this.voter = voter;
+        this.voterList = voters;
+        this.rank = rank;
+    }    
     public RankedPageNalla(String voter, ArrayList<VotingPageNalla> voters){
         this.voter = voter;
         this.voterList = voters;
+    }    
+    
+    public String getVoter() {
+        return voter;
+    }
+
+    public void setVoter(String voter) {
+        this.voter = voter;
+    }
+
+    public ArrayList<VotingPageNalla> getVoterList() {
+        return voterList;
+    }
+
+    public void setVoterList(ArrayList<VotingPageNalla> voterList) {
+        this.voterList = voterList;
+    }
+
+    @Override
+    public String toString(){
+        return this.voter +"<"+ this.rank +","+ voterList +">";
+    }
+
+    public double getRank() {
+        return this.rank;
     }
 }
--- Sushma Nalla/pagerank-nalla-java/src/main/java/edu/nwmsu/sec02grp1/nalla/VotingPageNalla.java
+++ Sushma Nalla/pagerank-nalla-java/src/main/java/edu/nwmsu/sec02grp1/nalla/VotingPageNalla.java
@@ -1,16 +1,51 @@
 package edu.nwmsu.sec02grp1.nalla;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.values.KV;
 
-public  class VotingPageNalla extends DoFn<KV<String,Iterable<String>>,KV<String,RankedPageNalla>>{
+public  class VotingPageNalla extends DoFn<KV<String,Iterable<String>>,KV<String,RankedPageNalla>> implements Serializable{
     String voterName;
     int contributorVotes;
+    double pageRank = 1.0;
+    public VotingPageNalla(String voterName,Integer contributorVotes2, double pageRank){
+        this.voterName = voterName;
+        this.contributorVotes = contributorVotes2;      
+        this.pageRank = pageRank;  
+    }
+
     public VotingPageNalla(String voterName,Integer contributorVotes2){
         this.voterName = voterName;
-        this.contributorVotes = contributorVotes2;        
+        this.contributorVotes = contributorVotes2;      
+   
     }
+    
+    public String getVoterName() {
+        return voterName;
+    }
+    public void setVoterName(String voterName) {
+        this.voterName = voterName;
+    }
+    public int getContributorVotes() {
+        return contributorVotes;
+    }
+    public void setContributorVotes(int contributorVotes) {
+        this.contributorVotes = contributorVotes;
+    }
+    @Override
+    public String toString() {
+        return "voterName = "+ voterName +", Page rank = "+this.pageRank +" ContributorVotes = " + contributorVotes;
+    }
+
+    public double getPageRank() {
+        return this.pageRank;
+    }
+    public void setPageRank(double pageRank){
+        this.pageRank = pageRank;
+    }
+
+
 }
