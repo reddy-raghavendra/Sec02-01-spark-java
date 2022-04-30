b00d54de1bf919c4b4dd673e88b84cbefb76812e
saikumar438 <saikumar.mylavarapu1996@gmail.com>
2022-04-29 19:22:43 -0500

    added all the files

--- Saikumar Mylavarapu/pagerank/src/main/java/edu/nwmsu/sec02grp1/mylavarapu/RankedPageMylavarapu.java
+++ Saikumar Mylavarapu/pagerank/src/main/java/edu/nwmsu/sec02grp1/mylavarapu/RankedPageMylavarapu.java
@@ -5,15 +5,41 @@ import java.util.ArrayList;
 
 public class RankedPageMylavarapu implements Serializable{
     String voter;
+    double rank = 1.0;
     ArrayList<VotingPageMylavarapu> voterList = new ArrayList<>();
     
+    public RankedPageMylavarapu(String voter,double rank, ArrayList<VotingPageMylavarapu> voters){
+        this.voter = voter;
+        this.voterList = voters;
+        this.rank = rank;
+    }    
     public RankedPageMylavarapu(String voter, ArrayList<VotingPageMylavarapu> voters){
         this.voter = voter;
         this.voterList = voters;
+    }    
+    
+    public String getVoter() {
+        return voter;
+    }
+
+    public void setVoter(String voter) {
+        this.voter = voter;
+    }
+
+    public ArrayList<VotingPageMylavarapu> getVoterList() {
+        return voterList;
+    }
+
+    public void setVoterList(ArrayList<VotingPageMylavarapu> voterList) {
+        this.voterList = voterList;
     }
 
     @Override
-    public String toString() {
-        return voter + voterList;
+    public String toString(){
+        return this.voter +"<"+ this.rank +","+ voterList +">";
+    }
+
+    public double getRank() {
+        return this.rank;
     }
 }
